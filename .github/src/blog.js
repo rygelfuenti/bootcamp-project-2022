7acf3af379a1560a037a786f2e09792d2e2c3c46
Rygel Fuentiblanca <fuentiblancarygel@gmail.com>
2024-10-17 22:10:06 -0700

    blog.html and src/blog.js and src/blog.ts and AboutMe.html and CodingJourney.html

--- .github/src/blog.js
+++ .github/src/blog.js
@@ -2,18 +2,18 @@ var blogs = [
     {
         title: "My Coding Journey",
         date: "10/16/24",
-        description: "My coding journey so far has been challenging but a very eye opening experienced.  Prior to my first classes at Cal Poly, I had no experience whatsoever of programming, and I felt rather overwhelmed jumping into code on my first day of school.  From learning the basics like HTML, CSS, and Javascript to diving deeper into code like Data Structures and Algorithms, its easy to say that learning wasn't easy.  There have been many moments where I faced doubt, asking myself 'Am I Built For This', but I have also experienced major satisfaction from solving hard problems, and finally getting my code to work.   As I continue to grow and learn more about code, I hope to be the best developer I can be! ",
+        description: "Click For A Look Into My Coding Journey",
         image: "giphy.gif",
         imageAlt: "Gif of Dog ",
-        slug: "Coding-Journey",
+        slug: "CodingJourney",
     },
     {
         title: "About Me",
         date: "10/16/24",
-        description: " I am a 2nd year Computer Engineering major, but I am hoping to switch to Computer Science this quarter. I am local as I am from Santa Maria, which is only about a 30-minute drive from Cal Poly. In my free time, I like to play video games, watch anime (Favorite: One Piece), go to the gym, and do some occasional fishing!",
+        description: "Click To Learn More About Me",
         image: "gif.gif",
         imageAlt: "Random Gif",
-        slug: "About Me",
+        slug: "AboutMe",
     }
 ];
 function displayBlogs(blogs) {
@@ -22,6 +22,9 @@ function displayBlogs(blogs) {
         //Create A Div For Each Blog Post
         var blogDiv = document.createElement('div');
         blogDiv.classList.add('blog-post');
+        //Create Anchor For Each Blog Page
+        var anchorElement = document.createElement('a');
+        anchorElement.href = "".concat(blog.slug, ".html");
         //Create H1
         var H1Element = document.createElement('h1');
         H1Element.textContent = blog.title;
@@ -33,9 +36,10 @@ function displayBlogs(blogs) {
         var PElement = document.createElement('p');
         PElement.textContent = blog.description;
         // Append Everything to BlogDiv
-        blogDiv.appendChild(H1Element);
-        blogDiv.appendChild(ImageElement);
-        blogDiv.appendChild(PElement);
+        anchorElement.appendChild(H1Element);
+        anchorElement.appendChild(ImageElement);
+        anchorElement.appendChild(PElement);
+        blogDiv.appendChild(anchorElement);
         //Append Entire BlogDiv to blogContainer
         blogContainer.appendChild(blogDiv);
     });
