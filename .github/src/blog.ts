41fe59e0618ea7b5c046d9a61fc26705025653a7
Rygel Fuentiblanca <fuentiblancarygel@gmail.com>
2024-10-17 17:52:26 -0700

    feat:blog.js and blog.ts and .gitignore and blog.html and contact.html gif.gif and giphy.gif and index.html and package-lock.json and package.json and portfolio.html and README.md resume.html and Resume.pdf styles.css and tsconfig.json and website.jpg

---
+++ .github/src/blog.ts
@@ -0,0 +1,58 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+};
+
+const blogs: Blog[] = [
+    {
+    title: "My Coding Journey",
+    date: "10/16/24",
+    description:"My coding journey so far has been challenging but a very eye opening experienced.  Prior to my first classes at Cal Poly, I had no experience whatsoever of programming, and I felt rather overwhelmed jumping into code on my first day of school.  From learning the basics like HTML, CSS, and Javascript to diving deeper into code like Data Structures and Algorithms, its easy to say that learning wasn't easy.  There have been many moments where I faced doubt, asking myself 'Am I Built For This', but I have also experienced major satisfaction from solving hard problems, and finally getting my code to work.   As I continue to grow and learn more about code, I hope to be the best developer I can be! ",
+    image: "giphy.gif",
+    imageAlt: "Gif of Dog ",
+    slug: "Coding-Journey",
+    },
+    {
+    title: "About Me",
+    date: "10/16/24",
+    description:" I am a 2nd year Computer Engineering major, but I am hoping to switch to Computer Science this quarter. I am local as I am from Santa Maria, which is only about a 30-minute drive from Cal Poly. In my free time, I like to play video games, watch anime (Favorite: One Piece), go to the gym, and do some occasional fishing!",
+    image: "gif.gif",
+    imageAlt: "Random Gif",
+        slug: "About Me",
+    }
+]
+function displayBlogs(blogs: {title: string, image: string, description: string}[]) {
+    const blogContainer = document.getElementById("blog-container")!;
+
+    blogs.forEach(blog => {
+        //Create A Div For Each Blog Post
+       const blogDiv = document.createElement('div');
+       blogDiv.classList.add('blog-post') 
+
+       //Create H1
+       const H1Element = document.createElement('h1');
+       H1Element.textContent = blog.title;
+
+       //Create Images
+       const ImageElement = document.createElement('img');
+       ImageElement.src = blog.image;
+       ImageElement.alt = blog.title;
+
+       //Create P Tags for Blog Description
+       const PElement = document.createElement('p')
+       PElement.textContent = blog.description;
+
+       // Append Everything to BlogDiv
+       blogDiv.appendChild(H1Element);
+       blogDiv.appendChild(ImageElement);
+       blogDiv.appendChild(PElement);
+
+       //Append Entire BlogDiv to blogContainer
+       blogContainer.appendChild(blogDiv);
+    })
+}
+displayBlogs(blogs)
